14dd740daec4be0c001a8a0918af90e9bd7e0638
Kristian Hannula <kristian.hannula@proton.me>
2023-09-24 01:00:47 +0300

    Fixed asset filepaths

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,21 @@
+import githublogo from '../assets/GitHub-Logo.png'
+import linkedinlogo from '../assets/linkedin-logo.png'
+
 const Navbar = () => {
   return (
-    <div className="flex w-1/1 border-b-2 h-14 font-semibold items-center justify-center">
+    <div className="flex border-b-2 w-1/1 h-14 font-semibold items-center justify-center">
       <a href="https://github.com/khan5ula">
         <img
-          className="object-cover h-4 max-w-10 hover:opacity-60 cursor-pointer mx-3"
-          src="src/assets/GitHub-Logo.png"
+          className="object-cover h-3 max-w-10 hover:opacity-80 cursor-pointer mx-3"
+          src={githublogo}
           alt="GitHub logo"
         />
       </a>
       <a href="https://www.linkedin.com/in/kristian-hannula-1800a924a/">
         <img
-          className="object-cover h-5 max-w-10 hover:opacity-60 cursor-pointer mx-3 mt-0.4"
-          src="src/assets/linkedin-logo.png"
-          alt="LinkedIn logo"
+          className="object-cover h-4 max-w-10 hover:opacity-80 cursor-pointer mx-3 mt-0.4"
+          src={linkedinlogo}
+          alt="GitHub logo"
         />
       </a>
     </div>
