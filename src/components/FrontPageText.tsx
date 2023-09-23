94741d7c2b6f53052167ac1762c53ae7b1399280
Kristian Hannula <kristian.hannula@proton.me>
2023-09-24 02:06:49 +0300

    another typo

--- src/components/FrontPageText.tsx
+++ src/components/FrontPageText.tsx
@@ -2,7 +2,7 @@ const FrontPageText = () => (
   <div className="mx-10 text-base max-w-prose mt-10 md:mt-0">
     <h1 className="mb-6 mt-6 text-center md:text-left">Hey 👋</h1>
     <p>
-      My name is Kristian Hannula. Thanks for visiting my website! Tough this
+      My name is Kristian Hannula. Thanks for visiting my website! Although this
       site is definitely not as cool as the{' '}
       <span className="underline underline-offset-4 hover:decoration-1">
         <a href="https://en.wikipedia.org/wiki/Dragon_Ball_(manga)">DBZ</a>
