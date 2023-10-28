eddc6a78fae8ff16192290a786e6e838c83a818e
Kristian <kristian.hannula@proton.me>
2023-10-28 13:14:21 +0300

    Changed logos on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,19 @@
-import githublogo from '../assets/GitHub-Logo.png'
+import githublogo from '../assets/github-logo.png'
 import linkedinlogo from '../assets/linkedin-logo.png'
 
 const Footer = () => (
-  <footer className="flex justify-center items-center h-8 bg-white">
+  <footer className="flex justify-center items-center h-14 bg-white">
     <p className="text-sm">© 2023 Kristian Hannula</p>
     <a href="https://github.com/khan5ula">
       <img
-        className="object-cover h-3 max-w-10 hover:opacity-50 cursor-pointer mx-3"
+        className="object-cover h-5 hover:opacity-90 cursor-pointer ml-5"
         src={githublogo}
         alt="GitHub logo"
       />
     </a>
     <a href="https://www.linkedin.com/in/kristian-hannula-1800a924a/">
       <img
-        className="object-cover h-4 max-w-10 hover:opacity-50 cursor-pointer mx-3 mt-0.4"
+        className="object-cover h-5 max-w-10 hover:opacity-90 cursor-pointer ml-6 mt-0.4"
         src={linkedinlogo}
         alt="LinkedIn logo"
       />
